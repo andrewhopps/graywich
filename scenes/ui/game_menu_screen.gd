276eb8c87967ef72dea2b4ee9587cb7637a91212
Andrew Hopps <andrewhopps@gmail.com>
2025-05-13 15:40:15 -0400

    Revert "Test"

--- scenes/ui/game_menu_screen.gd
+++ scenes/ui/game_menu_screen.gd
@@ -1,15 +1,12 @@
 extends CanvasLayer
 
-@onready var settings_game_button: Button = $MarginContainer/MarginContainer2/VBoxContainer/SettingsGameButton as Button
 @onready var save_game_button: Button = $MarginContainer/MarginContainer2/VBoxContainer/SaveGameButton
-@onready var options_menu: OptionsMenu = $Options_Menu as OptionsMenu
-@onready var margin_container: MarginContainer = $MarginContainer as MarginContainer
+
 
 
 func _ready() -> void:
 	save_game_button.disabled = !SaveGameManager.allow_save_game
 	save_game_button.focus_mode = SaveGameManager.allow_save_game if Control.FOCUS_ALL else Control.FOCUS_NONE
-	handle_connecting_signals()
 
 func _on_start_game_button_pressed() -> void:
 	GameManager.start_game() 
@@ -19,18 +16,10 @@ func _on_start_game_button_pressed() -> void:
 func _on_save_game_button_pressed() -> void:
 	SaveGameManager.save_game() # Replace with function body.
 
-func on_options_pressed() -> void:
-	margin_container.visible = false
-	options_menu.set_process(true)
-	options_menu.visible = true
 
 func _on_exit_game_button_pressed() -> void:
 	GameManager.exit_game() # Replace with function body.
 
-func on_exit_options_menu() -> void:
-	margin_container.visible = true
-	options_menu.visible = false
 
-func handle_connecting_signals() -> void:
-	settings_game_button.button_down.connect(on_options_pressed)
-	options_menu.exit_options_menu.connect(on_exit_options_menu)
+func _on_settings_game_button_pressed() -> void:
+	pass # Replace with function body.
